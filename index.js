0e39493a4aa2c32fb0a152b2cd3dc24c1e812bd5
charles <cyphersmae@gmail.com>
2025-06-24 11:02:09 +0300

    completed first class lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+// Code your solution in this file
+
+const returnFirstTwoDrivers = function(drivers) {
+return drivers.slice(0,2)
+}
+const returnLastTwoDrivers = function(drivers) {
+return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+function createFareMultiplier(multiplier) {
+return function(fare) {
+return fare *multiplier
+  }
+}
+const fareDoubler =createFareMultiplier(2)
+const fareTripler =createFareMultiplier(3)
+function selectDifferentDrivers(drivers, driverFunction) {
+return driverFunction(drivers)
+}
+const drivers = ['Antonia','Nuru','Amari','Mo']
+     console.log(returnFirstTwoDrivers(drivers))
+console.log(returnLastTwoDrivers(drivers))
+   console.log(fareDoubler(10))
+console.log(fareTripler(10))
+  console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers))
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers)); 
+
+
